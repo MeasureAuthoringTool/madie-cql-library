c4691d50d3d8f306e1f51d29f603572da76eae77
Nate Moraca <nate.moraca@semanticbits.com>
2022-03-10 13:05:36 -0500

    add cql library schema validator with rules for library name; ref #MAT-3903

---
+++ src/models/CqlLibrarySchemaValidator.ts
@@ -0,0 +1,11 @@
+import * as Yup from "yup";
+
+export const CqlLibrarySchemaValidator = Yup.object().shape({
+  libraryName: Yup.string()
+    .max(255, "Library name cannot be more than 255 characters.")
+    .required("Library name is required.")
+    .matches(
+      /^[A-Z][a-zA-Z0-9]*$/,
+      "Library name must start with an upper case letter, followed by alpha-numeric character(s) and must not contain spaces or other special characters."
+    ),
+});
