2bd7f0062a4418f3ac3df0b88e7b8679e186c9a9
Prateek Keerthi <prateek.keerthi@PeKeMBPro.local>
2022-03-14 17:45:02 -0400

    removed unused components

--- src/components/cqlLibraryRoutes/CqlLibraryRoutes.tsx
+++ src/components/cqlLibraryRoutes/CqlLibraryRoutes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import CqlLibraryLanding from "../cqlLibraryLanding/CqlLibraryLanding";
 
 export function CqlLibraryRoutes() {
