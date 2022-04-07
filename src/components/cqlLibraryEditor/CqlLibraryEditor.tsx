cef634f05e908a8e6a217e526375ffd578106989
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-04-07 10:42:52 -0400

    validate Cql

--- src/components/cqlLibraryEditor/CqlLibraryEditor.tsx
+++ src/components/cqlLibraryEditor/CqlLibraryEditor.tsx
@@ -1,16 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "styled-components/macro";
-import { MadieEditor } from "@madie/madie-editor";
+import { EditorAnnotation, MadieEditor } from "@madie/madie-editor";
+import * as _ from "lodash";
+import useElmTranslationServiceApi, {
+  ElmTranslation,
+  ElmTranslationError,
+} from "../../api/useElmTranslationServiceApi";
 
 export interface CqlLibraryEditorProps {
+  displayAnnotations: boolean;
+  setDisplayAnnotations: (val: boolean) => void;
+  setElmTranslationError: (val: string) => void;
+  setSuccessMessage: (val: string) => void;
   value: string;
   onChange: (val: string) => void;
 }
 
-const CqlLibraryEditor = ({ value, onChange }: CqlLibraryEditorProps) => {
+export const mapElmErrorsToAceAnnotations = (
+  errors: ElmTranslationError[]
+): EditorAnnotation[] => {
+  let annotations: EditorAnnotation[] = [];
+  if (errors && _.isArray(errors) && errors.length > 0) {
+    annotations = errors.map((error: ElmTranslationError) => ({
+      row: error.startLine - 1,
+      column: error.startChar,
+      type: error.errorSeverity.toLowerCase(),
+      text: `ELM: ${error.startChar}:${error.endChar} | ${error.message}`,
+    }));
+  }
+  return annotations;
+};
+
+const CqlLibraryEditor = ({
+  displayAnnotations,
+  setDisplayAnnotations,
+  setElmTranslationError,
+  setSuccessMessage,
+  value,
+  onChange,
+}: CqlLibraryEditorProps) => {
+  const elmTranslationServiceApi = useElmTranslationServiceApi();
+  const [elmAnnotations, setElmAnnotations] = useState<EditorAnnotation[]>([]);
+
+  const updateElmAnnotations = async (cql: string): Promise<ElmTranslation> => {
+    if (cql && cql.trim().length > 0) {
+      const data = await elmTranslationServiceApi.translateCqlToElm(cql);
+      const elmAnnotations = mapElmErrorsToAceAnnotations(
+        data?.errorExceptions
+      );
+      setElmAnnotations(elmAnnotations);
+    } else {
+      setElmAnnotations([]);
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    if (displayAnnotations) {
+      updateElmAnnotations(value).catch((err) => {
+        console.error("An error occurred while translating CQL to ELM", err);
+        setElmTranslationError("Unable to translate CQL to ELM!");
+        setElmAnnotations([]);
+      });
+    }
+  }, [value, displayAnnotations]);
+
   const handleMadieEditorValue = (val: string) => {
+    setElmTranslationError(undefined);
+    setDisplayAnnotations(false);
+    setSuccessMessage(undefined);
     onChange(val);
-    // TODO: validate CQL to ELM
   };
 
   return (
@@ -18,7 +77,8 @@ const CqlLibraryEditor = ({ value, onChange }: CqlLibraryEditorProps) => {
       <MadieEditor
         onChange={(val: string) => handleMadieEditorValue(val)}
         value={value}
-        height="814px"
+        inboundAnnotations={displayAnnotations ? elmAnnotations : []}
+        height="780px"
       />
     </>
   );
