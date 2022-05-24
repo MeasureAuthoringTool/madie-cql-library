a5519384ae02319c7f0a803244b7fbb390fdd291
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-05-24 11:29:28 -0400

    fixing build issue

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { mergeWithRules } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 const path = require("path");
+const styledComponents = require("styled-components");
 
 module.exports = (webpackConfigEnv, argv) => {
   const protocol = webpackConfigEnv.protocol
