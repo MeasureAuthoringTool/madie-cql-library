0bb1f75d339f927e41ea64ec3f492b5dfc6b26b3
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2024-10-30 10:50:51 -0700

    MAT-7722 change editor height for temp fix

--- src/components/cqlLibraryEditor/CqlLibraryEditor.tsx
+++ src/components/cqlLibraryEditor/CqlLibraryEditor.tsx
@@ -49,7 +49,7 @@ const CqlLibraryEditor = ({
         onChange={onChange}
         value={value}
         inboundAnnotations={inboundAnnotations}
-        height="calc(100vh - 135px)"
+        height="calc(100vh - 420px)"
         readOnly={readOnly}
         setOutboundAnnotations={setOutboundAnnotations}
       />
