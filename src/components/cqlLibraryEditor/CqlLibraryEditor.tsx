8d3ccc1c7bc81f76c3a54de3c98dc5c0bd5af741
Ethan Kaplan <47760891+ethankaplan@users.noreply.github.com>
2024-10-31 14:12:31 -0700

    Undo previous

--- src/components/cqlLibraryEditor/CqlLibraryEditor.tsx
+++ src/components/cqlLibraryEditor/CqlLibraryEditor.tsx
@@ -49,7 +49,7 @@ const CqlLibraryEditor = ({
         onChange={onChange}
         value={value}
         inboundAnnotations={inboundAnnotations}
-        height="calc(100vh - 420px)"
+        height="calc(100vh - 135px)"
         readOnly={readOnly}
         setOutboundAnnotations={setOutboundAnnotations}
       />
