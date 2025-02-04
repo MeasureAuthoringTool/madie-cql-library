dec59fbc4f475e9cc797173d60e859306f403627
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2025-02-04 13:41:43 -0800

    MAT-6183 lint

--- src/components/common/CreateNewLibraryDialog.tsx
+++ src/components/common/CreateNewLibraryDialog.tsx
@@ -45,7 +45,7 @@ const CreateNewLibraryDialog: React.FC<TestProps> = ({
 
   let modelOptions = Object.keys(Model);
   const featureFlags = useFeatureFlags();
-    modelOptions = modelOptions.filter((option) => option !== "QICORE_6_0_0");
+  modelOptions = modelOptions.filter((option) => option !== "QICORE_6_0_0");
 
   // fetch organizations DB using measure service and sorts alphabetically
   useEffect(() => {
