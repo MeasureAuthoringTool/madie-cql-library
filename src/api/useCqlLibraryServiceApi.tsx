28f082698373ff0970605edd2ed4713b9a5f5087
Prateek Keerthi <prateekkeerthi7@gmail.com>
2025-03-10 13:12:14 -0400

    MAT-7194 added version library button and functionality

--- src/api/useCqlLibraryServiceApi.tsx
+++ src/api/useCqlLibraryServiceApi.tsx
@@ -3,6 +3,7 @@ import useServiceConfig from "./useServiceConfig";
 import { ServiceConfig } from "./ServiceContext";
 import { CqlLibrary } from "@madie/madie-models";
 import useOktaTokens from "../hooks/useOktaTokens";
+import { AxiosResponse } from "axios";
 
 export class CqlLibraryServiceApi {
   constructor(private baseUrl: string, private getAccessToken: () => string) {}
@@ -68,7 +69,10 @@ export class CqlLibraryServiceApi {
     );
   }
 
-  async createVersion(id: string, isMajor: boolean): Promise<void> {
+  async createVersion(
+    id: string,
+    isMajor: boolean
+  ): Promise<AxiosResponse<CqlLibrary>> {
     return await axios.put(
       `${this.baseUrl}/cql-libraries/version/${id}?isMajor=${isMajor}`,
       {},
