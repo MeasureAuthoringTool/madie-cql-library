7b61187bca4163f17cfcc8e0e186d2874a9acf3d
Prateek Keerthi <prateekkeerthi7@gmail.com>
2025-06-12 10:32:11 -0400

    MAT-8456 fixed all option search issue

--- src/components/cqlLibraryLanding/CqlLibraryLanding.tsx
+++ src/components/cqlLibraryLanding/CqlLibraryLanding.tsx
@@ -129,6 +129,11 @@ function CqlLibraryLanding() {
           abortController.current.signal
         )
         .then((data) => {
+          const updatedLimit =
+            limit === "All" && data.totalElements < 50 ? 50 : limit;
+          navigate(`?tab=${tab}&page=1&limit=${updatedLimit}`, {
+            replace: true,
+          });
           setPageProps(data);
         })
         .catch((error) => {
