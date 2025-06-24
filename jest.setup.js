b4fc6e736a875c25c4dd4b497ec438a692cc7541
Rohit Kandimalla <rohit.kandimalla@icf.com>
2025-06-24 12:11:29 -0400

    MAT-8026 removing console.error and warn statement while running unit test cases

--- jest.setup.js
+++ jest.setup.js
@@ -4,6 +4,16 @@ global.System = {
   import: jest.fn(mockImport),
 };
 
+beforeAll(() => {
+  jest.spyOn(console, "error").mockImplementation(() => {});
+  jest.spyOn(console, "warn").mockImplementation(() => {});
+});
+
+afterAll(() => {
+  console.error.mockRestore();
+  console.warn.mockRestore();
+});
+
 function mockImport(importName) {
   if (importName === "@madie/madie-util") {
     return Promise.resolve(util);
